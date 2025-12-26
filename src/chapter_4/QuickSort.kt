c6ed52689fd5ea988142eba076c265f8bcb28d9b
developer <fallene123@gmail.com>
2025-12-26 16:06:59 +0300

    GrokingAlgorithm

--- src/chapter_4/QuickSort.kt
+++ src/chapter_4/QuickSort.kt
@@ -3,6 +3,15 @@ package chapter_4
 fun quickSort(numbersList: List<Int>): List<Int> {
     if (numbersList.size < 2) {
         return numbersList
+    } else {
+        val pivot = numbersList[0]
+        val equals = numbersList.filter { it == pivot }
+        val less = numbersList.drop(1).filter { it < pivot }
+        val greater = numbersList.drop(1).filter { it > pivot }
+        return quickSort(less) + equals + quickSort(greater)
     }
-    TODO("Дочитай и доделай быструю сортировку")
+}
+
+fun main() {
+    println(quickSort(listOf(10, 5, 2, 3)))
 }
